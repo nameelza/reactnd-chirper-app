08c299392cdebf45e228897ce307738ef9c0c53a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-28 15:47:03 -0700

    readMe

---
+++ src/actions/shared.js
@@ -0,0 +1,12 @@
+import { getInitialData } from "../utils/api";
+import { receiveUsers } from "../actions/users";
+import { receiveTweets } from "../actions/tweets";
+
+export function handleInitalData() {
+  return (dispatch) => {
+    return getInitialData.then(({users, tweets}) => {
+        dispatch(receiveUsers(users));
+        dispatch(receiveTweets(tweets));
+    })
+  }
+}

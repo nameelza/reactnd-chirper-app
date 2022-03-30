9c8074e909a89385d5e512a1afcd23cae7b4e3b9
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-30 16:35:30 -0700

    loadingBarComponent

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
-import { LoadingBar } from "react-redux-loading";
+import LoadingBar from "react-redux-loading";
 
 class App extends Component {
   componentDidMount() {

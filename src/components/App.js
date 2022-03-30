174166fb54328b854cc3e608b5e61d275b3ea1f9
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-30 16:31:12 -0700

    loadingBarComponent

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import { LoadingBar } from "react-redux-loading";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    console.log("props", this.props);
-    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
+    return (
+      <div>
+        <LoadingBar />
+        {this.props.loading === true ? null : <Dashboard />}
+      </div>
+    )
   }
 }
 

8770bdc49d9e948e96892f5218a10c554e78f97f
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-30 11:21:05 -0700

    TweetComponent

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { formatTweet } from "../utils/helpers";
 
 class Tweet extends Component {
-    render() {
-        return (
-            <div className="tweet">
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="tweet">
+        <h3>Tweet</h3>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps ({authedUser, tweets, users}, {id}) {
+function mapStateToProps({ authedUser, tweets, users }, { id }) {
   const tweet = tweets[id];
   return {
-      authedUser,
-      tweet: tweet,
-  }
+    authedUser,
+    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+  };
 }
 
 export default connect()(Tweet);

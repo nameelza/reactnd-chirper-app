e76ce6a0c260d9ca0c20fe8021f9531e2a4f381d
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-30 11:50:40 -0700

    TweetComponent

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,7 @@ import { formatTweet } from "../utils/helpers";
 
 class Tweet extends Component {
   render() {
-      console.log(this.props)
+    console.log(this.props);
     return (
       <div className="tweet">
         <h3>Tweet</h3>
@@ -15,9 +15,11 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, tweets, users }, { id }) {
   const tweet = tweets[id];
+  const parentTweet = tweets[tweet.repluingTo];
+
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser),
+    tweet: formatTweet(tweet, users[tweet.author], authedUser, parentTweet),
   };
 }
 

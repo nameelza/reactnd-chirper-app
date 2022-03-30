82dd6c66b02858399153a2e820086bb4cd51d92a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-30 15:51:44 -0700

    TweetComponent

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { formatTweet } from "../utils/helpers";
+import { formatTweet, formatDate } from "../utils/helpers";
+import {
+  TiArrowBackOutline,
+  TiHeartOutline,
+  TiHeartFullOutline,
+} from "react-icons/ti";
 
 class Tweet extends Component {
+  toParent = (e, id) => {
+    e.preventDefault();
+    // todo: Redirect to the parent Tweet
+  }
   render() {
+    const { tweet } = this.props;
+
+    if (tweet === null) {
+      return <p>This tweet doesn't exist</p>;
+    }
+
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      hasLiked,
+      likes,
+      replies,
+      id,
+      parent,
+    } = tweet;
+
     console.log(this.props);
     return (
       <div className="tweet">
-        <h3>Tweet</h3>
+        <img src={avatar} alt={`Avatar of ${name}`} className="avatar"/>
+        <div className="tweet-info">
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className="replying-to" onClick={(e) => this.toParent(e, parent.id)}>
+                Replying to @{parent.author}
+              </button>
+            )}
+        </div>
       </div>
     );
   }
@@ -15,11 +51,13 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, tweets, users }, { id }) {
   const tweet = tweets[id];
-  const parentTweet = tweets[tweet.repluingTo];
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser, parentTweet),
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null,
   };
 }
 

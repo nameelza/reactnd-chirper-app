5838116b9b0bec3690c0c57796cadce8a48e761a
nameelza <82921161+nameelza@users.noreply.github.com>
2022-03-31 16:29:50 -0700

    NewTweetComponent

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 
 class NewTweet extends Component {
+  state = {
+    text: "",
+  };
+
+  handleChange = (e) => {
+    const text = e.target.value;
+
+    this.setState(() => {
+      text;
+    });
+  };
+
   render() {
     return <div>New Tweet</div>;
   }

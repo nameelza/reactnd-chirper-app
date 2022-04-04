50fcaddb23794a8fd7ebda63272f501925ac27a2
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-04 16:10:05 -0700

    Nav.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import NewTweet from "./NewTweet";
 import TweetPage from "./TweetPage";
 import LoadingBar from "react-redux-loading";
+import Nav from "./Nav";
 
 class App extends Component {
   componentDidMount() {
@@ -12,10 +14,14 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true ? null : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
-      </div>
+      <Router>
+        <div>
+          <LoadingBar />
+          {this.props.loading === true ? null : (
+            <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
+          )}
+        </div>
+      </Router>
     );
   }
 }

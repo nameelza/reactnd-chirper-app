b297b34e1ce8a473d7b69d7712beae7a84e041b9
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-04 12:37:56 -0700

    TweetPageComponent

--- src/components/TweetPage.js
+++ src/components/TweetPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 class TweetPage extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h3>tweet page</h3>
@@ -13,6 +14,15 @@ class TweetPage extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, props) {
   const { id } = props.match.params;
+
+  return {
+    id,
+    replies: !tweets[id]
+      ? []
+      : tweets[id].replies.sort(
+          (a, b) => tweets[b].timestamp - tweets[a].timestamp
+        ),
+  };
 }
 
-export default connect()(TweetPage);
+export default connect(mapStateToProps)(TweetPage);

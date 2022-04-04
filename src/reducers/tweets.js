869c803c6e4a2fe9b13283cef7f3d872e606f064
nameelza <82921161+nameelza@users.noreply.github.com>
2022-04-04 15:49:17 -0700

    AddTweetReducer

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -14,28 +14,30 @@ export default function tweets(state = {}, action) {
           ...state[action.id],
           likes:
             action.hasLiked === true
-              ? state[action.id].likes.filter((uid) => uid !== action.authedUser)
+              ? state[action.id].likes.filter(
+                  (userId) => userId !== action.authedUser
+                )
               : state[action.id].likes.concat([action.authedUser]),
         },
       };
     case ADD_TWEET:
-      const { tweet } = action
-      
-      let replyingTo = {}
+      const { tweet } = action;
+
+      let replyingTo = {};
       if (tweet.replyingTo !== null) {
         replyingTo = {
           [tweet.replyingTo]: {
             ...state[tweet.replyingTo],
-            replies: state[tweet.replyingTo].replies.concat([tweet.id])
-          }
-        }
+            replies: state[tweet.replyingTo].replies.concat([tweet.id]),
+          },
+        };
       }
 
       return {
         ...state,
         [tweet.id]: tweet,
         ...replyingTo,
-      }
+      };
     default:
       return state;
   }
